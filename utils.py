10054f73c05a5305758150139b5c1e329e4ff93c
PAITxBOT <143090682+PAITxBOT@users.noreply.github.com>
2023-12-26 12:10:02 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -127,7 +127,7 @@ async def get_poster(query, bulk=False, id=False, file=None):
 async def is_check_admin(bot, chat_id, user_id):
     try:
         member = await bot.get_chat_member(chat_id, user_id)
-        return member.status in [enums.ChatMemberStatus.MEMBER]
+        return member.status in [enums.ChatMemberStatus.MEMBER, enums.ChatMemberStatus.OWNER]
     except:
         return False
 
